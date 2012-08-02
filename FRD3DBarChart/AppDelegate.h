60839d28e412c1d7f1d9833b3d57e87d7433beb1
Sebastien Windal <sebastien@windal.net>
2012-08-01 20:53:31 -0400

    Initial Commit

---
+++ FRD3DBarChart/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FRD3DBarChart
+//
+//  Created by Sebastien Windal on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FRD3DBarChart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FRD3DBarChart
+//
+//  Created by Sebastien Windal on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
